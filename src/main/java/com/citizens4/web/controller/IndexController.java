ef90f8ca2f5917b272ab7dc931d1ba1cc4c2f5c1
reggiechapple <reginald.chapple@yahoo.com>
2020-08-21 18:53:36 -0700

    setup layout

--- src/main/java/com/citizens4/web/controller/IndexController.java
+++ src/main/java/com/citizens4/web/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.citizens4.web.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     
     @GetMapping(value="/")
-    public String homePage() {
+    public String homePage(Model model) {
+        model.addAttribute("message", "Hello everyone, we are go to back to Spring with together");
+        model.addAttribute("date", new Date());
         return "index";
     }
 
